35ae8910b40ed30ce8b6e404ae513e25007a83e5
YS-Jeong-99 <besthcy9908@gmail.com>
2022-07-05 13:57:02 +0900

    test

--- lib/weekly_date_picker.dart
+++ lib/weekly_date_picker.dart
@@ -153,10 +153,13 @@ class _WeeklyDatePickerState extends State<WeeklyDatePicker> {
       child: GestureDetector(
         onTap: () => widget.changeDay(dateTime),
         child: Padding(
-          padding: const EdgeInsets.all(4.0),
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Container(
             // Bugfix, the transparent container makes the GestureDetector fill the Expanded
             color: Colors.red,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
